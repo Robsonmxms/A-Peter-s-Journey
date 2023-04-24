f22d95d9279eaaa2db7728fff7134c649f5c9c6e
Robson Lima Lopes <robson060814@hotmail.com>
2023-04-24 11:26:00 -0300

    inserting different sizes for bricks

--- APetersJourney/Utils/Maze.swift
+++ APetersJourney/Utils/Maze.swift
@@ -13,6 +13,21 @@ class Maze {
     private let brickWidth: CGFloat
     private let size: CGSize
     private let floorWallsProportion: Double
+    private let bigBricksTextures: [SKTexture] = [
+        SKTexture(imageNamed: "rock1"),
+        SKTexture(imageNamed: "rock2"),
+        SKTexture(imageNamed: "rock3"),
+        SKTexture(imageNamed: "rock8"),
+        SKTexture(imageNamed: "rock9")
+    ]
+
+    private let smallBrickstextures: [SKTexture] = [
+        SKTexture(imageNamed: "rock4"),
+        SKTexture(imageNamed: "rock5"),
+        SKTexture(imageNamed: "rock6"),
+        SKTexture(imageNamed: "rock7"),
+        SKTexture(imageNamed: "rock10")
+    ]
 
     init(
         size: CGSize,
@@ -101,22 +116,88 @@ class Maze {
     }
 
     func getWallsAsSKSpriteNode() -> [SKSpriteNode] {
-
         var spriteNodes: [SKSpriteNode] = []
 
-        for point in walls {
-            let wallBrick = SKSpriteNode(
-                imageNamed: "wall"
-            )
-            wallBrick.size = CGSize(width: brickWidth, height: brickWidth)
-
-            wallBrick.position = point
-
-            wallBrick.physicsBody = SKPhysicsBody(rectangleOf: wallBrick.size)
-            wallBrick.physicsBody?.isDynamic = false
-            spriteNodes.append(wallBrick)
-        }
+        buildBigBrick(spriteNodes: &spriteNodes)
+        buildSmallBrick(spriteNodes: &spriteNodes)
 
         return spriteNodes
     }
+
+    private func buildBigBrick(spriteNodes: inout [SKSpriteNode]) {
+        let totalBricksPositions = walls.count
+        var randomBricksPositions = [CGPoint]()
+        let howManyBigBricks = Int(walls.count/3)
+
+        let startIndex = Int.random(
+            in: 0...(totalBricksPositions - howManyBigBricks)
+        )
+
+        for index in startIndex..<(startIndex + howManyBigBricks) {
+            randomBricksPositions.append(walls[index])
+        }
+
+        for point in randomBricksPositions {
+            let topPoint: CGPoint = CGPoint(
+                x: point.x+brickWidth,
+                y: point.y
+            )
+
+            let trailingPoint: CGPoint = CGPoint(
+                x: point.x,
+                y: point.y+brickWidth
+            )
+
+            let topTrailingPoint: CGPoint = CGPoint(
+                x: point.x+brickWidth,
+                y: point.y+brickWidth
+            )
+
+            let haveTop: Bool = walls.contains(topPoint)
+            let haveTrailing: Bool = walls.contains(trailingPoint)
+            let haveTopTrailing: Bool = walls.contains(topTrailingPoint)
+            let haveSelfPoint: Bool = walls.contains(point)
+
+            let texture = bigBricksTextures.randomElement()!
+
+            let wallBrick = SKSpriteNode(
+                texture: texture
+            )
+
+            if haveTop && haveTrailing && haveTopTrailing && haveSelfPoint {
+                walls.removeAll {$0 == topPoint}
+                walls.removeAll {$0 == trailingPoint}
+                walls.removeAll {$0 == topTrailingPoint}
+                walls.removeAll {$0 == point}
+
+                wallBrick.size = CGSize(width: brickWidth*2, height: brickWidth*2)
+                wallBrick.position = CGPoint(
+                    x: point.x + brickWidth/2,
+                    y: point.y + brickWidth/2
+                )
+                wallBrick.physicsBody = SKPhysicsBody(texture: texture, size: wallBrick.size)
+//                wallBrick.physicsBody = SKPhysicsBody(rectangleOf: wallBrick.size)
+                wallBrick.physicsBody?.isDynamic = false
+                wallBrick.physicsBody?.collisionBitMask = 1
+                spriteNodes.append(wallBrick)
+            }
+        }
+    }
+
+    private func buildSmallBrick(spriteNodes: inout [SKSpriteNode]) {
+        for point in walls {
+            let wallBrick = SKSpriteNode(
+                texture: smallBrickstextures.randomElement()!
+            )
+
+                wallBrick.size = CGSize(width: brickWidth, height: brickWidth)
+
+                wallBrick.position = point
+
+                wallBrick.physicsBody = SKPhysicsBody(rectangleOf: wallBrick.size)
+                wallBrick.physicsBody?.isDynamic = false
+                spriteNodes.append(wallBrick)
+        }
+    }
+
 }
--- APetersJourney/Utils/SphereSKNode.swift
+++ APetersJourney/Utils/SphereSKNode.swift
@@ -8,32 +8,33 @@
 import SpriteKit
 import SceneKit
 
+// TODO: After inserting all 3d objects scenes, rename SphereSKNode to Object3dSKNode
 class SphereSKNode: SKNode {
     init(
         brickWidth: CGFloat,
-        sphereSCNNode: SCNNode,
+        sphereSCNScene: SCNScene,
         position: CGPoint
     ) {
         super.init()
 
         let sphereSK3DNode = SK3DNode()
-        let scene = SCNScene()
-        scene.rootNode.addChildNode(sphereSCNNode)
 
         sphereSK3DNode.viewportSize = CGSize(
             width: brickWidth*0.8,
             height: brickWidth*0.8
         )
 
-        sphereSK3DNode.scnScene = scene
+        sphereSK3DNode.scnScene = sphereSCNScene
 
         self.addChild(sphereSK3DNode)
 
         self.position = position
 
+        // !!!: Maybe you have to modify the physicsBody after inserting all 3d objects scenes
         self.physicsBody = SKPhysicsBody(
             circleOfRadius: (brickWidth/2)*0.6
         )
+
         self.physicsBody?.mass = 4.5
         self.physicsBody?.allowsRotation = false
         self.physicsBody?.isDynamic = true
